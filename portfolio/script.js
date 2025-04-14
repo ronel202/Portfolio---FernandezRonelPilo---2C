5307b21468c8d14b4ad446296ff67848cf8e8ad0
ronel202 <mronel472@gmail.com>
2025-04-14 11:07:22 +0800

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const showPopup = document.getElementById("showPopup");
+    const closePopup = document.getElementById("closePopup");
+    const popup = document.getElementById("popup");
+
+    showPopup.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup.style.display = "block";
+    });
+
+    closePopup.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup.style.display = "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const showPopup1 = document.getElementById("showPopup1");
+    const closePopup1 = document.getElementById("closePopup1");
+    const popup1 = document.getElementById("popup1");
+
+    showPopup1.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup1.style.display = "block";
+    });
+
+    closePopup1.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup1.style.display = "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const showPopup2 = document.getElementById("showPopup2");
+    const closePopup2 = document.getElementById("closePopup2");
+    const popup2 = document.getElementById("popup2");
+
+    showPopup2.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup2.style.display = "block";
+    });
+
+    closePopup2.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup2.style.display = "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const showPopup3 = document.getElementById("showPopup3");
+    const closePopup3 = document.getElementById("closePopup3");
+    const popup3 = document.getElementById("popup3");
+
+    showPopup3.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup3.style.display = "block";
+    });
+
+    closePopup3.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup3.style.display = "none";
+    });
+});
+ document.addEventListener("DOMContentLoaded", function() {
+    const showPopup4 = document.getElementById("showPopup4");
+    const closePopup4 = document.getElementById("closePopup4");
+    const popup4 = document.getElementById("popup4");
+
+    showPopup4.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup4.style.display = "block";
+    });
+
+    closePopup4.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        popup4.style.display = "none";
+    });
+});
